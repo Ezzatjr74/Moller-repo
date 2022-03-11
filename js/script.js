9ab1cf05a678f5713918f2eb9ca8e5d90152e43b
Ezzatjr74 <omare6412@gmail.com>
2022-03-11 03:01:58 +0200

    moller project

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+
+});
+$('.owl-carousel').owlCarousel({
+    items:3,
+    loop:false,
+    margin:80,
+    merge:true,
+    responsive:{
+        678:{
+            mergeFit:true
+        },
+        1000:{
+            mergeFit:false
+        }
+    }
+});
+var x = 0;
+
+$(window).onscroll(
+    if (window.scrollTo() ==  )
+)
+
+
+
+
+
